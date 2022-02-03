2084b49fdc75cd51527e0f67fb47ebaee5689ce8
IFloresCh <iflores@institumvm.cat>
2022-02-03 16:09:53 +0100

    upload

--- FuncionesPython/main.py
+++ FuncionesPython/main.py
@@ -2,8 +2,8 @@ def multiplica(a,b):
     print(a * b)
 
 def main():
-    a=int(input("Introdueix a"))
-    b=int(input("Introdueix b"))
+    a=int(input("Introdueix a: "))
+    b=int(input("Introdueix b: "))
     multiplica(a,b)
 
 if __name__ == "__main__":
