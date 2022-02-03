e952be97185e2a516b3114f657d241c468733073
IFloresCh <94060293+IFloresCh@users.noreply.github.com>
2022-02-03 16:07:24 +0100

    Add files via upload

---
+++ FuncionesPython/main.py
@@ -0,0 +1,10 @@
+def multiplica(a,b):
+    print(a * b)
+
+def main():
+    a=int(input("Introdueix a"))
+    b=int(input("Introdueix b"))
+    multiplica(a,b)
+
+if __name__ == "__main__":
+    main()
