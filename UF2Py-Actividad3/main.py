117fca3926fb2e62e12365225eb1ebcf8615c589
IFloresCh <iflores@institumvm.cat>
2022-02-04 15:45:24 +0100

    upload

---
+++ UF2Py-Actividad3/main.py
@@ -0,0 +1,11 @@
+
+import functions as f
+
+def main():
+    bin, n = 0, 0
+    x=f.validate(n)
+    print("El número en binario es :",f.decimaltobinary(x))
+
+if __name__ == "__main__":
+    main()
+    
