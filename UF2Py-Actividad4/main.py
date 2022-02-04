15951e10205e1cf92fe24f85a88e534cf397dadd
IFloresCh <iflores@institumvm.cat>
2022-02-04 16:13:49 +0100

    upload

--- UF2Py-Actividad4/main.py
+++ UF2Py-Actividad4/main.py
@@ -1,3 +1,20 @@
 import functions as f
 
-def main 
+def main():
+    avg_apr, avg_sus = [], []
+    for x in range(14):
+        num = f.validate(num);
+
+        if (num >= 5 and num <= 10):
+            avg_apr.append(num)
+            
+        
+        if(num >= 0 and num < 5):
+            avg_sus.append(num)
+
+    print("passed : %f\n",avg_apr)
+    print("failed : %f\n",avg_sus)
+
+
+if __name__ == "__main__":
+    main()
