0ff4a9ec72ae8ecb11d370707f4d483e58738c7a
IFloresCh <iflores@institumvm.cat>
2022-02-10 16:03:44 +0100

    123

---
+++ UF2Py-Actividad12/main.py
@@ -0,0 +1,10 @@
+import functions as f
+def main():
+   
+    num = f.validate()
+    f.lists(num)
+    lista = f.lists(num)
+    
+    print(lista)
+if __name__ == '__main__':
+    main()
---
+++ UF2Py-Actividad12/test.py
@@ -0,0 +1,6 @@
+def main():
+    lista2 = [x for x in range(31) if x % 2 == 0]
+    print(lista2)
+
+if __name__ == '__main__':
+    main()
